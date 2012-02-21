260170ae983f2563fa41da87e9fb056d6ebd7377
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-02-21 09:32:49 +0100

    Adding git aliases and completion

---
+++ git/aliases.zsh
@@ -0,0 +1,12 @@
+# The rest of my fun git aliases
+alias ga='git add -A'
+alias gb='git branch'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gd='git diff'
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
