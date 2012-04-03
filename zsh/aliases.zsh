6fe4532966303d628ba479d1773d5e04e50d092a
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-04-03 11:47:22 +0200

    Handling color in ls for BSD and Unix

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias reload!='. ~/.zshrc'
 
-alias ls='ls -h'
+# Find the option for using colors in ls, depending on the version: Linux or BSD
+ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty -h' || alias ls='ls -Gh'
 alias ll='ls -l'
 alias l='ll'
 alias la='ls -la'
