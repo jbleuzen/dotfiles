5b33570101396bdbec33cdd71829f2b78412bd23
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-04-25 04:52:18 +0200

    Adding an alias to colored grep output

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,7 @@
 alias reload!='. ~/.zshrc'
 
+alias grep='grep --color'
+
 # Find the option for using colors in ls, depending on the version: Linux or BSD
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty -h' || alias ls='ls -Gh'
 alias ll='ls -l'
