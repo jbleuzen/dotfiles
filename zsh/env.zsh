49233e30c1c529e31844d19c301c707b3f6a6b01
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-04-26 07:10:32 +0200

    Restoring rvm config in the right spot

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,3 +2,6 @@ export EDITOR='vim'
 
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/sbin
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+# RVM configuration
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
