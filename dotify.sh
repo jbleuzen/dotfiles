8a6b8131603f7593b7313a456be61f4624157f68
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-07-06 23:33:42 +0200

    Fixing missing bracket

--- dotify.sh
+++ dotify.sh
@@ -5,41 +5,42 @@
 
 function link {
 
-skip_all=false
-overwrite_all=false
-backup_all=false
+	skip_all=false
+	overwrite_all=false
+	backup_all=false
 
-for file in `find ~/.dotfiles -name "*.symlink" | grep -v '\.git'`; do
-	filename=$(basename $file)
-	filename=~/.${filename%.*}
-
-	overwrite=false
-	backup=false
-
-	if [ -e $filename ] || [ -h $filename ]; then
-
-		if [ $skip_all == "false" ] && [ $overwrite_all == "false" ] && [ $backup_all == "false" ]; then
-			echo "File already exists: $filename, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all"
-			read ACTION
-
-			case $ACTION in
-				O) overwrite_all=true;;
-				o) overwrite=true;;
-				B) backup_all=true;;
-				b) backup=true;;
-				S) skip_all=true;;
-				s) continue;;
-			esac
+	for file in `find ~/.dotfiles -name "*.symlink" | grep -v '\.git'`; do
+		filename=$(basename $file)
+		filename=~/.${filename%.*}
+	
+		overwrite=false
+		backup=false
+	
+		if [ -e $filename ] || [ -h $filename ]; then
+	
+			if [ $skip_all == "false" ] && [ $overwrite_all == "false" ] && [ $backup_all == "false" ]; then
+				echo "File already exists: $filename, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all"
+				read ACTION
+	
+				case $ACTION in
+					O) overwrite_all=true;;
+					o) overwrite=true;;
+					B) backup_all=true;;
+					b) backup=true;;
+					S) skip_all=true;;
+					s) continue;;
+				esac
+			fi
+			if [ $overwrite_all == "true" ] || [ $overwrite == "true" ]; then
+				rm -rf $filename
+			fi
+			if [ $backup_all == "true" ] || [ $backup == "true" ]; then
+				mv $filename ${filename}.backup 	
+			fi
 		fi
-		if [ $overwrite_all == "true" ] || [ $overwrite == "true" ]; then
-			rm -rf $filename
-		fi
-		if [ $backup_all == "true" ] || [ $backup == "true" ]; then
-			mv $filename ${filename}.backup 	
-		fi
-	fi
-	ln -s $file $filename 2> /dev/null
-done
+		ln -s $file $filename 2> /dev/null
+	done
+}
 
 # Install bundle
 vim +BundleInstall +qall
