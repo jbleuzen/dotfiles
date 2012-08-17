750aad1081c7aafb2d37c7a24e8770521a7c6808
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-08-17 17:39:54 +0200

    Adding username in git log Adding an alias for tmux

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,7 +6,7 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gd='git diff'
 alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,3 +1,4 @@
+alias t="tmux"
 alias ta="tmux attach -t"
 alias tk="tmux kill-session -t"
 alias tl="tmux ls"
