b3827169ed2ab175bec46c3dcb16a7fb47f1a87b
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-09-20 12:48:24 +0200

    Adding quick session switch with tm

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -32,3 +32,26 @@ function ssh() {
   tmux rename-window zsh
 }
 
+
+# 2011-10-19: tmux shortcut for creating/attaching named sessions
+tm() {
+    [[ -z "$1" ]] && { echo "usage: tm <session>" >&2; return 1; }
+    tmux has -t $1 
+	if [ $? -eq "0" ] ;then
+		tmux switch -t $1
+	else 
+		TMUX="" # To prevent message with 
+		tmux new-session -s $1 -d
+		tmux switch -t $1
+	fi
+}
+
+# 2011-10-19
+# stolen from completion function _tmux
+function __tmux-sessions() {
+	local expl
+	local -a sessions
+	sessions=( ${${(f)"$(command tmux list-sessions)"}/:[ $'\t']##/:} )
+	_describe -t sessions 'sessions' sessions "$@"
+}
+compdef __tmux-sessions tm
