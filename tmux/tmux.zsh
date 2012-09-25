b1015714077fd47e7827f501c84e9c071ea8dde3
Johan Bleuzen <johan.bleuzen@gmail.com>
2012-09-25 14:34:14 +0200

    Fixing tm when not in tmux session

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -36,13 +36,23 @@ function ssh() {
 # 2011-10-19: tmux shortcut for creating/attaching named sessions
 tm() {
     [[ -z "$1" ]] && { echo "usage: tm <session>" >&2; return 1; }
+
     tmux has -t $1 
 	if [ $? -eq "0" ] ;then
-		tmux switch -t $1
+		if [ -z $TMUX ] ;then
+			tmux attach -t $1
+		else
+			tmux switch -t $1
+		fi
 	else 
-		TMUX="" # To prevent message with 
-		tmux new-session -s $1 -d
-		tmux switch -t $1
+		if [ -z $TMUX ] ;then
+			tmux new-session -s $1
+			tmux attach -t $1
+		else
+			TMUX="" # To prevent message with 
+			tmux new-session -s $1 -d
+			tmux switch -t $1
+		fi
 	fi
 }
 
