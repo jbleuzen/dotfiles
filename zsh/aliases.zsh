9e9bdfb34ba215d07511ce019175402d5f62f29a
Johan Bleuzen <johan.bleuzen@gmail.com>
2013-06-24 22:58:17 +0200

    Restoring a working vim alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,9 +5,12 @@ alias grep='grep --color'
 
 # Find the option for using colors in ls, depending on the version: Linux or BSD
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty -h' || alias ls='ls -Gh'
-alias ll='ls -l'
 alias l='ll'
+alias ll='ls -l'
 alias la='ls -la'
+
+[[ -s /usr/local/Cellar/vim/7.3.923/bin/vim ]] && alias vi='/usr/local/Cellar/vim/7.3.923/bin/vim'
+[[ -s /usr/local/Cellar/vim/7.3.923/bin/vim ]] && alias vim='/usr/local/Cellar/vim/7.3.923/bin/vim'
 
 # Navigation
 alias cd..="cd .."
