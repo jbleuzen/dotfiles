d03d4e42b6a0a9dd873cab36a4f8651737c61da2
Johan Bleuzen <johan.bleuzen@gmail.com>
2013-06-24 23:04:42 +0200

    Fixing last bug with linux aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,8 +9,8 @@ alias l='ll'
 alias ll='ls -l'
 alias la='ls -la'
 
-[[ -s /usr/local/Cellar/vim/7.3.923/bin/vim ]] && alias vi='/usr/local/Cellar/vim/7.3.923/bin/vim'
-[[ -s /usr/local/Cellar/vim/7.3.923/bin/vim ]] && alias vim='/usr/local/Cellar/vim/7.3.923/bin/vim'
+[[ -s /usr/local/Cellar/vim/7.3.923/bin/vim ]] &> /dev/null && alias vi='/usr/local/Cellar/vim/7.3.923/bin/vim'
+[[ -s /usr/local/Cellar/vim/7.3.923/bin/vim ]] &> /dev/null && alias vim='/usr/local/Cellar/vim/7.3.923/bin/vim'
 
 # Navigation
 alias cd..="cd .."
