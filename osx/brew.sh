070c6058e321b30258b20b51b6aea0dccf91ce41
Johan Bleuzen <johan.bleuzen@gmail.com>
2013-07-01 18:00:49 +0200

    Adding a brew setup script

---
+++ osx/brew.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew install ack
+brew install git
+brew install ngrep
+brew install nload
+brew install node
+brew install reattach-to-user-namespace
+brew install tmux
+brew install tmux-vim-select-pane
+brew install vim --override-system-vi --with-python
