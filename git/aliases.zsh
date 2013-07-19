dda3fed2c5dea583506253b134d05c2a1c97dd68
Johan Bleuzen <johan.bleuzen@gmail.com>
2013-07-19 14:40:13 +0200

    New git alias gf

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,6 +5,7 @@ alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gd='git diff'
+alias gf='git fetch'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
