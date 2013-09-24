526d9b3c645c841222d212092983c1dd6d00a13e
Johan Bleuzen <johan@bleuzen.fr>
2013-09-24 04:26:13 +0200

    Remove duplicate option

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -10,7 +10,6 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 unsetopt CORRECT # disable autocorrect
