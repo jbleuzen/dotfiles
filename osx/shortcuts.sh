bd95c1c15e83ecdf38f812a280157a18812f6b78
Johan Bleuzen <johan@bleuzen.fr>
2013-12-29 23:01:36 +0100

    Adding shortcuts configuration

---
+++ osx/shortcuts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Global shortcuts
+defaults write -g NSUserKeyEquivalents -dict-add "Select Next Tab" "@~\\U2190"
+defaults write -g NSUserKeyEquivalents -dict-add "Select Previous Tab" "@~\\U2192"
+defaults write -g NSUserKeyEquivalents -dict-add "Précédent" "@\\U2190"
+defaults write -g NSUserKeyEquivalents -dict-add "Suivant" "@\\U2192"
+defaults write -g NSUserKeyEquivalents -dict-add "Sélectionner l'onglet précédent" "@~\\U2190"
+defaults write -g NSUserKeyEquivalents -dict-add "Sélectionner l'onglet suivant" "@~\\U2192"
+
+# Finder shortcuts
+#defaults write com.apple.Finder NSUserKeyEquivalents -dict-add "Masquer la barre latérale" "@\$d"
+#defaults write com.apple.Finder NSUserKeyEquivalents -dict-add "Afficher la barre latérale" "@\$d"
+
+### With Mavericks you must clear prefs cache
+killall cfprefsd
+
+### Restart Finder
+killall Finder
