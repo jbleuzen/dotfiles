426ca8bec0228e3efbc9f98a730c14ad57d64da7
Johan Bleuzen <johan@bleuzen.fr>
2014-01-20 21:49:51 +0100

    Fix invalid shortcuts

--- osx/shortcuts.sh
+++ osx/shortcuts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Global shortcuts
-defaults write -g NSUserKeyEquivalents -dict-add "Select Next Tab" "@~\\U2190"
-defaults write -g NSUserKeyEquivalents -dict-add "Select Previous Tab" "@~\\U2192"
+defaults write -g NSUserKeyEquivalents -dict-add "Select Previous Tab" "@~\\U2190"
+defaults write -g NSUserKeyEquivalents -dict-add "Select Next Tab" "@~\\U2192"
 defaults write -g NSUserKeyEquivalents -dict-add "Précédent" "@\\U2190"
 defaults write -g NSUserKeyEquivalents -dict-add "Suivant" "@\\U2192"
 defaults write -g NSUserKeyEquivalents -dict-add "Sélectionner l'onglet précédent" "@~\\U2190"
