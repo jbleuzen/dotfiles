f762b99d7e458f43ff827dce008fe02ee3f4828c
Johan Bleuzen <johan@bleuzen.fr>
2014-02-18 21:19:58 +0100

    Add iTunes shortcut

--- osx/shortcuts.sh
+++ osx/shortcuts.sh
@@ -11,6 +11,8 @@ defaults write -g NSUserKeyEquivalents -dict-add "Sélectionner l'onglet suivant
 # Finder shortcuts
 defaults write com.apple.Finder NSUserKeyEquivalents -dict-add "Masquer la barre latérale" "@\$d"
 defaults write com.apple.Finder NSUserKeyEquivalents -dict-add "Afficher la barre latérale" "@\$d"
+defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Masquer la barre latérale" "@\$d"
+defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Afficher la barre latérale" "@\$d"
 
 ### With Mavericks you must clear prefs cache
 killall cfprefsd
