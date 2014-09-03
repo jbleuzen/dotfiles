5ae97e2801d0107e4efc8cfa140a16b5aa8a5b77
Johan Bleuzen <johan@bleuzen.fr>
2014-09-03 13:42:26 +0200

    Change gca to git commit --amend

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,7 +2,7 @@
 alias ga='git add -A'
 alias gb='git branch'
 alias gc='git commit'
-alias gca='git commit -a'
+alias gca='git commit --amend'
 alias gco='git checkout'
 alias gd='git diff'
 alias gf='git fetch'
