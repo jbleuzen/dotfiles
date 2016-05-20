18ea76b44c2f9f5f89c1528fbdeb7bad4f65c88f
Johan Bleuzen <johan@bleuzen.fr>
2016-05-20 16:19:05 +0200

    Add alias to hide whitespace when diff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,6 +5,7 @@ alias gc='git commit'
 alias gca='git commit --amend'
 alias gco='git checkout'
 alias gd='git diff'
+alias gdi='git diff --ignore-space-change'
 alias gf='git fetch'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
