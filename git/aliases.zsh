9e8642f9ea48e5801fb7ebf3702271824bd98c37
Johan Bleuzen <johan@blzn.dev>
2021-07-13 14:23:49 +0200

    feat: Add git alias to push and create a MR

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -16,6 +16,8 @@ alias gf='git fetch'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
+alias gpo="git push origin"
+alias gpmr="git push -o merge_request.create"
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gu="git reset --soft HEAD^"
