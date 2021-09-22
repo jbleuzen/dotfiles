788675c63d736d31ec6d3ca120613b9562aa03f5
Johan Bleuzen <johan@bleuzen.fr>
2021-09-22 07:26:44 +0200

    feat: Update env for homebrew for M1

--- zsh/_env.zsh
+++ zsh/_env.zsh
@@ -18,13 +18,17 @@ export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;32m'
 export PAGER='less'
 
-export PATH=/usr/local/opt/coreutils/libexec/gnubin
-export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin
+export PATH=$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
+export PATH=$PATH:$HOMEBREW_PREFIX/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin
 export PATH=$PATH:$HOME/.dotfiles/bin # Add dotfiles bin to path
-#export PATH=$HOME/.rbenv/shims:$PATH # Add RVM to PATH for scripting
-export PATH=$PATH:/usr/local/share/npm/bin # Add npm binaries
+export PATH=$PATH:$HOMEBREW_PREFIX/share/npm/bin # Add npm binaries
 
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# Brew options
+eval "$(/opt/homebrew/bin/brew shellenv)"
+# Don't display emoji after brew install
+export HOMEBREW_NO_EMOJI=1
 
 # Disable tmux queuing for macOS Sierra
 export EVENT_NOKQUEUE=1
@@ -32,22 +36,15 @@ export EVENT_NOKQUEUE=1
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git,dist,__snapshots__'
 
 # rbenv if exists
-[[ -s "/usr/local/bin/rbenv" ]] && eval "$(rbenv init -)"
+[[ -s "${HOMEBREW_PREFIX}/bin/rbenv" ]] && eval "$(rbenv init -)"
 
 # Load z if exists
-[[ -s "/usr/local/etc/profile.d/z.sh" ]] && . "/usr/local/etc/profile.d/z.sh"
+[[ -s "${HOMEBREW_PREFIX}/etc/profile.d/z.sh" ]] && . "${HOMEBREW_PREFIX}/etc/profile.d/z.sh"
 
 # Load fnm if exists
 [[ -s `which fnm` ]] && eval "$(fnm env)"
 
 # Load dircolors if exists
 [[ -s `which dircolor` ]] && eval "$(dircolors ~/.dircolors)"
-
-# Load docker-machine if exists
-# Slow down the shell startup
-#[[ -s `which docker-machine` ]] && [[ `docker-machine status` == "Running" ]] && eval "$(docker-machine env default)"
-
-# added by travis gem
-#[ -f $HOME/.travis/travis.sh ] && source $HOME/.travis/travis.sh
 
 [[ -f ./.secrets.zsh ]] && source .secrets.zsh
