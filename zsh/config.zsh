02dcacbfb08df24975b02420edfb94cd996a8668
Johan Bleuzen <johan@bleuzen.fr>
2022-01-28 23:52:11 +0100

    feat: Prevent extra space after RPROMPT

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -16,6 +16,8 @@ unsetopt CORRECT # disable autocorrect
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
+ZLE_RPROMPT_INDENT=0
+
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
