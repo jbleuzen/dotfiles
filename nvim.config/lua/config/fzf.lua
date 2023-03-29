a1dc30f45d34e1228479a4ef76d4010ba3334e2c
Johan Bleuzen <johan@bleuzen.fr>
2023-03-30 00:37:39 +0200

    feat: Update config of fzf-lua to get previous UI

--- nvim.config/lua/config/fzf.lua
+++ nvim.config/lua/config/fzf.lua
@@ -1,16 +1,22 @@
 local actions = require'fzf-lua.actions'
 
 require('fzf-lua').setup({
+  global_resume       = true,
+  files = {
+    prompt       = '❯ ',
+    multiprocess = true, -- run command in a separate process
+    git_icons    = true, -- show git icons?
+    file_icons   = false, -- show file icons?
+    color_icons  = true,
+  },
   actions = {
-    files = {
-      -- instead of the default action 'actions.file_edit_or_qf'
-      -- it's important to define all other actions here as this
-      -- table does not get merged with the global defaults
-      ["default"]       = actions.file_edit,
-      ["ctrl-s"]        = actions.file_split,
-      ["ctrl-v"]        = actions.file_vsplit,
-      ["ctrl-t"]        = actions.file_tabedit,
-      ["ctrl-q"]         = actions.file_sel_to_qf,
-    },
+    -- instead of the default action 'actions.file_edit_or_qf'
+    -- it's important to define all other actions here as this
+    -- table does not get merged with the global defaults
+    ["default"]       = actions.file_edit,
+    ["ctrl-s"]        = actions.file_split,
+    ["ctrl-v"]        = actions.file_vsplit,
+    ["ctrl-t"]        = actions.file_tabedit,
+    ["ctrl-q"]         = actions.file_sel_to_qf,
   },
 })
