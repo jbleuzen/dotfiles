fdde272bc462ea9e61ce3cec254ed229c0a20b85
Johan Bleuzen <johan@bleuzen.fr>
2023-10-24 12:37:08 +0200

    chore: Update fzf config for preview images

--- nvim.config/lua/config/fzf.lua
+++ nvim.config/lua/config/fzf.lua
@@ -1,7 +1,7 @@
 local actions = require'fzf-lua.actions'
 
 require('fzf-lua').setup({
-  global_resume       = true,
+  global_resume = true,
   files = {
     prompt       = '❯ ',
     multiprocess = true, -- run command in a separate process
@@ -9,14 +9,25 @@ require('fzf-lua').setup({
     file_icons   = false, -- show file icons?
     color_icons  = true,
   },
+  previewers = {
+    builtin = {
+      extensions = {
+        -- neovim terminal only supports `viu` block output
+        ["png"] = { "viu", "-b" },
+        ["jpg"] = { "viu", "-b" }
+      }      
+    }
+  },
   actions = {
-    -- instead of the default action 'actions.file_edit_or_qf'
-    -- it's important to define all other actions here as this
-    -- table does not get merged with the global defaults
-    ["default"]       = actions.file_edit,
-    ["ctrl-s"]        = actions.file_split,
-    ["ctrl-v"]        = actions.file_vsplit,
-    ["ctrl-t"]        = actions.file_tabedit,
-    ["ctrl-q"]         = actions.file_sel_to_qf,
+    files = {
+      -- instead of the default action 'actions.file_edit_or_qf'
+      -- it's important to define all other actions here as this
+      -- table does not get merged with the global defaults
+      ["default"]       = actions.file_edit,
+      ["ctrl-s"]        = actions.file_split,
+      ["ctrl-v"]        = actions.file_vsplit,
+      ["ctrl-t"]        = actions.file_tabedit,
+      ["ctrl-q"]         = actions.file_sel_to_qf,
+    },
   },
 })
