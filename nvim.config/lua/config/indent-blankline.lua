13b1bf719bb11bcfec9f8650498c6b180b6ce73b
Johan Bleuzen <johan@bleuzen.fr>
2023-10-24 12:37:42 +0200

    chore: Update indent-blank-line config

--- nvim.config/lua/config/indent-blankline.lua
+++ nvim.config/lua/config/indent-blankline.lua
@@ -1,14 +1,15 @@
-vim.cmd [[highlight IndentBlanklineIndent1 guibg=#111111 gui=nocombine]]
+ vim.cmd [[highlight IndentBlanklineIndent1 guibg=#111111 gui=nocombine]]
 
-require("indent_blankline").setup {
-    char = "",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-    },
-    space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-    },
-    show_trailing_blankline_indent = false,
+local highlight = {
+    "IndentBlanklineIndent1",
+    "Whitespace",
+}
+
+require("ibl").setup { 
+  indent = { highlight = highlight, char = "" },
+  whitespace = {
+    highlight = highlight,
+    remove_blankline_trail = false,
+  },
+  scope = { enabled = true },
 }
