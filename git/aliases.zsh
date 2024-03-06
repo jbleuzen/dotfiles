8b1ede32e89ee3ccf4ce66a0cd416990c11d27e0
Johan Bleuzen <johan@bleuzen.fr>
2024-03-06 14:48:12 +0100

    feat: add alias to select git branch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,8 +2,11 @@
 if which fzf >/dev/null 2> /dev/null ; then
   alias fga='git ls-files -m -o --exclude-standard | fzf --height=50% --print0 -m --preview-window right:75%  --preview "git diff --color=always {}" | xargs -0 -t -o git add'
   alias fgco='git ls-files -m -o --exclude-standard | fzf --height=50% --print0 -m --preview-window right:75%  --preview "git diff --color=always {}" | xargs -0 -t -o git checkout'
-  alias fgb="git for-each-ref  refs/heads/ --format='%(HEAD) %(refname:short) - %(contents:subject) - %(authorname) %(committerdate:relative)' | grep -v '^\*' |fzf --height=40% --reverse --info=inline | awk '{print \$1}' | xargs git checkout"
-else 
+  #alias fgb="git for-each-ref  refs/heads/ --format='%(HEAD) %(refname:short) - %(contents:subject) - %(authorname) %(committerdate:relative)' | grep -v '^\*' |fzf --height=40% --reverse --info=inline --query=\$1 | awk '{print \$1}' | xargs git checkout"
+  fgb(){
+    echo $1
+    git for-each-ref  refs/heads/ --format='%(HEAD) %(refname:short) - %(contents:subject) - %(authorname) %(committerdate:relative)' | grep -v '^\*' |fzf --height=40% --reverse --info=inline --query=$1 | awk '{print \$1}' | xargs git checkout
+  }
 fi
 alias ga='git add -A'
 alias gba='git branch --all --color="always" --sort=authordate --format="%(color:red)%(refname:short);%(color:green)(%(authordate:relative));%(color:white)%(subject) %(color:blue)<%(authorname)>" "$@" | column -s ";" -t | cut -c 1-150'
@@ -26,6 +29,7 @@ alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gu="git reset --soft HEAD^"
 alias gsa="git stash apply"
 alias gss="git stash save"
+
 
 # Rewrite the default git branch with better branch list
 gb(){
