c356f8b83798e8604e58957c92265d2dfeef9f12
Johan Bleuzen <johan@bleuzen.fr>
2024-09-20 15:34:12 +0200

    feat: Add binding on nvim-tree

--- nvim.config/lua/config/tree.lua
+++ nvim.config/lua/config/tree.lua
@@ -1,5 +1,22 @@
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', 'u', api.tree.change_root_to_parent,        opts('Up'))
+  vim.keymap.set('n', '<Shift>Enter', api.tree.change_root_to_node,        opts('Up'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
 require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   reload_on_bufenter = true,
+  on_attach = my_on_attach,
   view = {
     signcolumn = "yes",
   },
