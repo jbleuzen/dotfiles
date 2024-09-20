387c1a84e5d73f9e41ebb8839c8d9defb8e8c4f2
Johan Bleuzen <johan@bleuzen.fr>
2024-09-20 15:33:03 +0200

    feat: Update tmux attach to rename Tab in WezTerm

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,6 +1,10 @@
 alias t="tmux && tmux rename-window zsh;"
-alias ta="tmux attach -t"
+#alias ta="tmux attach -t $0 && wezterm cli set-tab-title $0"
 alias tk="tmux kill-session -t"
 alias tl="tmux ls"
 alias tn="tmux new -s "
 alias ts="tmux switch -t "
+ta() {
+  [[ -s `which wezterm` ]] && wezterm cli set-tab-title $1
+  tmux attach -t $1
+}
