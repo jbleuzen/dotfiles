ef99d27940fce8dc3592fce2bbe1f2b24551b74b
Johan Bleuzen <johan@bleuzen.fr>
2024-09-29 21:35:25 +0200

    Feat: add a plugin to view colors in background

---
+++ nvim.config/lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-highlight-colors",
+	dependencies = {
+		"hrsh7th/nvim-cmp",
+	},
+  config = function ()
+
+    -- Ensure termguicolors is enabled if not already
+		vim.opt.termguicolors = true
+
+		-- Setup nvim-highlight-colors with required configurations
+		require("nvim-highlight-colors").setup({
+			render = "background", -- or 'foreground' or 'first_column'
+			enable_hex = true, -- Highlight hex colors
+			enable_short_hex = true, -- Highlight short hex colors
+			enable_rgb = true, -- Highlight rgb colors
+			enable_hsl = true, -- Highlight hsl colors with 'hsl' keyword
+			enable_var_usage = true, -- Highlight CSS variables
+			enable_named_colors = true,
+			enable_tailwind = true,
+		})
+
+		-- nvim-cmp integration (if needed)
+		local cmp = require("cmp")
+		cmp.setup({
+			-- Other configurations can be added here
+			formatting = {
+				format = require("nvim-highlight-colors").format,
+			},
+		})
+	end,
+}
