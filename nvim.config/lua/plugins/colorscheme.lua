1828adee9ef542274668127fa889bbf560b060d2
Johan Bleuzen <johan@bleuzen.fr>
2024-09-30 09:18:47 +0200

    chore: Update some colors

--- nvim.config/lua/plugins/colorscheme.lua
+++ nvim.config/lua/plugins/colorscheme.lua
@@ -6,13 +6,42 @@ return {
     local monokai = require('monokai')
     local palette = monokai.soda
 
+    --[[
+     palette = {
+        name = 'monokai',
+        base1 = '#272a30',
+        base2 = '#26292C',
+        base3 = '#2E323C',
+        base4 = '#333842',
+        base5 = '#4d5154',
+        base6 = '#9ca0a4',
+        base7 = '#b1b1b1',
+        border = '#a1b5b1',
+        brown = '#504945',
+        white = '#f8f8f0',
+        grey = '#8F908A',
+        black = '#000000',
+        pink = '#f92672',
+        green = '#a6e22e',
+        aqua = '#66d9ef',
+        yellow = '#e6db74',
+        orange = '#fd971f',
+        purple = '#ae81ff',
+        red = '#e95678',
+        diff_add = '#3d5213',
+        diff_remove = '#4a0f23',
+        diff_change = '#27406b',
+        diff_text = '#23324d',
+    },
+    --]]
+
     monokai.setup {
       palette = {
-        base2 = "#000000"
+        base2 = palette.black
       },
       custom_hlgroups = {
         Normal = {
-          bg = "#000000"
+          bg = palette.black
         },
         NormalNC = {
           bg = "#1C1C1C",
@@ -44,7 +73,7 @@ return {
           bg = "#202020",
         },
         DiagnosticSignError = {
-          bg = "#202020",
+          bg = palette.black
         },
         -- nvim-tree
         NvimTreeNormal = {
@@ -58,11 +87,11 @@ return {
         },
         NvimTreeRootArrowOpen = {
           fg = palette.base8,
-          bg = "#00FF00"
+          bg = palette.green
         },
         NvimTreeRootFolder = {
           fg = palette.base8,
-          bg = "#00FF00"
+          bg = palette.green
         },
         NvimTreeFolderName = {
           fg = palette.green,
@@ -71,8 +100,8 @@ return {
           fg = palette.green,
         },
         NvimTreeEndOfBuffer = {
-          fg= "#000000",
-          bg = "#000000"
+          fg= palette.black,
+          bg = palette.black
         },
         NvimTreeOpenedFolderName = {
           fg = palette.green,
@@ -85,7 +114,7 @@ return {
         },
         NvimTreeGitDirty = {
           fg=palette.green,
-          bg="#000000"
+          bg= palette.black
         },
         -- FZF
         FZFLuaBorder = {
@@ -96,19 +125,19 @@ return {
           bg = "#1C1C1C"
         },
         TroubleTextError = {
-          fg = "#D75F87"
+          fg = palette.red
         },
         TroubleTextWarning = {
-          fg = "#D7D787"
+          fg = palette.yellow
         },
         TroubleTextHint = {
-          fg = "#5FD7FF"
+          fg = palette.aqua
         },
         TroubleFoldIcon = {
-          fg = "#87D700"
+          fg = palette.green
         },
         TroubleFile = {
-          fg = "#87D700"
+          fg = palette.green
         }
       }
     }
