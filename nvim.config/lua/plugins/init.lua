dcea5e1a54f94934ff299b81da95c67a9c7af4db
Johan Bleuzen <johan@bleuzen.fr>
2024-10-05 19:19:13 +0200

    feat: Add git blame feature

--- nvim.config/lua/plugins/init.lua
+++ nvim.config/lua/plugins/init.lua
@@ -30,8 +30,32 @@ return {
 
   {
     "j-hui/fidget.nvim",
+  },
+
+  {
+    'FabijanZulj/blame.nvim',
     opts = {
-      -- options
+      date_format = "%d/%m/%y",
+      virtual_style = "right_align",
+      -- views = {
+      --   window = window_view,
+      --   virtual = virtual_view,
+      --   default = window_view,
+      -- },
+      focus_blame = true,
+      merge_consecutive = false,
+      max_summary_width = 30,
+      colors = nil,
+      blame_options = nil,
+      commit_detail_view = "vsplit",
+      -- format_fn = formats.commit_date_author_fn,
+      mappings = {
+        commit_info = "i",
+        stack_push = "<TAB>",
+        stack_pop = "<BS>",
+        show_commit = "<CR>",
+        close = { "<esc>", "q" },
+      }
     },
   }
 
