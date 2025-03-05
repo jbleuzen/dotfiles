76360cb140fcc74f11f0131c9cc1c1c0788751e0
Johan Bleuzen <johan@bleuzen.fr>
2025-03-05 08:51:33 +0100

    feat: Disable snacks dasboard

--- nvim.config/lua/plugins/nvim-snacks.lua
+++ nvim.config/lua/plugins/nvim-snacks.lua
@@ -9,7 +9,7 @@ return {
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 			bigfile = { enabled = true },
-			dashboard = { enabled = true },
+			dashboard = { enabled = false },
 			indent = { enabled = true },
 			input = { enabled = true },
 			notifier = { enabled = true },
