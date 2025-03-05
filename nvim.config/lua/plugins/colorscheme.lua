916ff4622f441da4a666a91672ab81d8d4e7f362
Johan Bleuzen <johan@bleuzen.fr>
2025-03-05 08:50:39 +0100

    feat: Update nvim-tree behaviour

--- nvim.config/lua/plugins/colorscheme.lua
+++ nvim.config/lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-  -- Colorscheme
-  'tanvirtin/monokai.nvim',
-  priority = 1000,
-  config = function ()
-    local monokai = require('monokai')
-    local palette = monokai.soda
+	-- Colorscheme
+	"tanvirtin/monokai.nvim",
+	priority = 1000,
+	config = function()
+		local monokai = require("monokai")
+		local palette = monokai.soda
 
-    --[[
+		--[[
      palette = {
         name = 'monokai',
         base1 = '#272a30',
@@ -35,129 +35,129 @@ return {
     },
     --]]
 
-    monokai.setup {
-      palette = {
-        base2 = "#000000",
-      },
-      custom_hlgroups = {
-        Normal = {
-          bg = palette.black
-        },
-        NormalNC = {
-          bg = "#1C1C1C",
-        },
-        CursorLineNr = {
-          bg = "#303030",
-          fg = "#FF8700"
-        },
-        LineNr = {
-          bg = "#202020",
-          fg = "#666666",
-        },
-        SignColumn = {
-          bg = "#202020",
-          fg = "#666666",
-        },
-        CursorLineSign = {
-          bg = "#303030",
-          fg = "#BCBCBC",
-        },
-        -- lsp
-        DiagnosticSignInfo = {
-          bg = "#202020",
-        },
-        DiagnosticSignHint = {
-          bg = "#202020",
-        },
-        DiagnosticSignWarn = {
-          bg = "#202020",
-        },
-        DiagnosticSignError = {
-          bg = palette.black
-        },
-        -- nvim-tree
-        NvimTreeNormal = {
-          bg = "#000000"
-        },
-        NvimTreeNormalNC = {
-          bg = "#1C1C1C"
-        },
-        NvimTreeIndentMarker = {
-          fg= "#333333"
-        },
-        NvimTreeRootArrowOpen = {
-          fg = palette.base8,
-          bg = palette.green
-        },
-        NvimTreeRootFolder = {
-          fg = palette.base8,
-          bg = palette.green
-        },
-        NvimTreeFolderName = {
-          fg = palette.green,
-        },
-        NvimTreeFolderIcon = {
-          fg = palette.green,
-        },
-        NvimTreeEndOfBuffer = {
-          fg= palette.black,
-          bg = palette.black
-        },
-        NvimTreeOpenedFolderName = {
-          fg = palette.green,
-        },
-        NvimTreeSpecialFile = {
-          fg = palette.purple
-        },
-        NvimTreeSymlink = {
-          fg = palette.aqua,
-        },
-        NvimTreeGitDirty = {
-          fg=palette.green,
-          bg= palette.black
-        },
-        -- FZF
-        FZFLuaBorder = {
-          fg = "#666666"
-        },
-        -- Trouble 
-        TroubleNormalNC = {
-          bg = "#1C1C1C"
-        },
-        TroubleFilename = {
-          fg = palette.green
-        },
-        TroubleTextError = {
-          fg = palette.red
-        },
-        TroubleTextWarning = {
-          fg = palette.yellow
-        },
-        TroubleTextHint = {
-          fg = palette.aqua
-        },
-        TroubleFoldIcon = {
-          fg = palette.green
-        },
-        TroubleFile = {
-          fg = palette.green
-        }
-      }
-    }
+		monokai.setup({
+			palette = {
+				base2 = "#000000",
+			},
+			custom_hlgroups = {
+				Normal = {
+					bg = palette.black,
+				},
+				NormalNC = {
+					bg = "#1C1C1C",
+				},
+				CursorLineNr = {
+					bg = "#303030",
+					fg = "#FF8700",
+				},
+				LineNr = {
+					bg = "#202020",
+					fg = "#666666",
+				},
+				SignColumn = {
+					bg = "#202020",
+					fg = "#666666",
+				},
+				CursorLineSign = {
+					bg = "#303030",
+					fg = "#BCBCBC",
+				},
+				-- lsp
+				DiagnosticSignInfo = {
+					bg = "#202020",
+				},
+				DiagnosticSignHint = {
+					bg = "#202020",
+				},
+				DiagnosticSignWarn = {
+					bg = "#202020",
+				},
+				DiagnosticSignError = {
+					bg = palette.black,
+				},
+				-- nvim-tree
+				NvimTreeNormal = {
+					bg = "#000000",
+				},
+				NvimTreeNormalNC = {
+					bg = "#1C1C1C",
+				},
+				NvimTreeIndentMarker = {
+					fg = "#333333",
+				},
+				NvimTreeRootArrowOpen = {
+					fg = palette.base8,
+					bg = palette.green,
+				},
+				NvimTreeRootFolder = {
+					fg = palette.magenta,
+					bg = "#121212",
+				},
+				NvimTreeFolderName = {
+					fg = palette.green,
+				},
+				NvimTreeFolderIcon = {
+					fg = palette.green,
+				},
+				NvimTreeEndOfBuffer = {
+					fg = palette.black,
+					bg = palette.black,
+				},
+				NvimTreeOpenedFolderName = {
+					fg = palette.green,
+				},
+				NvimTreeSpecialFile = {
+					fg = palette.purple,
+				},
+				NvimTreeSymlink = {
+					fg = palette.aqua,
+				},
+				NvimTreeGitDirty = {
+					fg = palette.green,
+					bg = palette.black,
+				},
+				-- FZF
+				FZFLuaBorder = {
+					fg = "#666666",
+				},
+				-- Trouble
+				TroubleNormalNC = {
+					bg = "#1C1C1C",
+				},
+				TroubleFilename = {
+					fg = palette.green,
+				},
+				TroubleTextError = {
+					fg = palette.red,
+				},
+				TroubleTextWarning = {
+					fg = palette.yellow,
+				},
+				TroubleTextHint = {
+					fg = palette.aqua,
+				},
+				TroubleFoldIcon = {
+					fg = palette.green,
+				},
+				TroubleFile = {
+					fg = palette.green,
+				},
+			},
+		})
 
-    local cmd = vim.cmd
+		local cmd = vim.cmd
 
-    -- Change the background color of inactive pane/window
-    cmd [[
+		-- Change the background color of inactive pane/window
+		cmd([[
       autocmd FocusGained * hi Normal guibg=#000000 | hi NvimTreeNormal guibg=#000000 | hi TroubleNormal guibg=#000000
       autocmd FocusLost * hi Normal guibg=#1C1C1C | hi NvimTreeNormal guibg=#1C1C1C | hi NvimTreeEndOfBuffer guifg=#1C1C1C guibg=#1C1C1C | hi TroubleNormal guibg=#1C1C1C
-    ]]
+    ]])
 
-    vim.api.nvim_create_autocmd({ "BufEnter" }, {
-      pattern = "*",
-      callback = function(opts)
-        if vim.bo[opts.buf].filetype == "NvimTree" or vim.bo[opts.buf].filetype == "trouble" then
-          cmd[[
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			pattern = "*",
+			callback = function(opts)
+				if vim.bo[opts.buf].filetype == "NvimTree" or vim.bo[opts.buf].filetype == "trouble" then
+					cmd([[
           hi NvimTreeEndOfBuffer guifg=#000000 guibg=#000000
           hi NvimTreeGitDirty guibg=#000000
           hi NvimTreeSignColumn guifg=#000000 guibg=#000000
@@ -168,16 +168,16 @@ return {
           hi NvimTreeGitIgnored guibg=#000000
           hi NvimTreeGitStaged guibg=#000000
           hi TroubleNormal guibg=#000000 
-          ]]
-        end
-      end,
-    })
+          ]])
+				end
+			end,
+		})
 
-    vim.api.nvim_create_autocmd({ "BufLeave" }, {
-      pattern = "*",
-      callback = function(opts)
-        if vim.bo[opts.buf].filetype == "NvimTree" or vim.bo[opts.buf].filetype == "trouble" then
-          cmd[[
+		vim.api.nvim_create_autocmd({ "BufLeave" }, {
+			pattern = "*",
+			callback = function(opts)
+				if vim.bo[opts.buf].filetype == "NvimTree" or vim.bo[opts.buf].filetype == "trouble" then
+					cmd([[
           hi NvimTreeEndOfBuffer guifg=#1C1C1C guibg=#1C1C1C
           hi NvimTreeGitDirty guibg=#1C1C1C
           hi NvimTreeSignColumn guifg=#1C1C1C guibg=#1C1C1C
@@ -188,9 +188,9 @@ return {
           hi NvimTreeGitIgnored guibg=#1C1C1C
           hi NvimTreeGitStaged guibg=#1C1C1C
           hi TroubleNormal guibg=#1C1C1C
-          ]]
-        end
-      end,
-    })
-  end
+          ]])
+				end
+			end,
+		})
+	end,
 }
--- nvim.config/lua/plugins/nvim-tree.lua
+++ nvim.config/lua/plugins/nvim-tree.lua
@@ -1,57 +1,65 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require'nvim-tree'.setup {
-      reload_on_bufenter = true,
-      view = {
-        signcolumn = "yes",
-      },
-      actions = {
-        change_dir = {
-          restrict_above_cwd = false,
-        },
-      },
-      renderer = {
-        add_trailing = false,
-        special_files= { 'package.json' },
-        root_folder_label = false,
-        icons = {
-          git_placement = "signcolumn",
-          show={
-            file = false,
-            folder = false,
-            folder_arrow = true,
-            git = true,
-            bookmarks = false,
-          },
-          glyphs = {
-            default = "",
-            symlink = "",
-            bookmark = "",
-            git = {
-              unstaged = "●",
-              staged = "+",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "?",
-              deleted = "-",
-              ignored = "◌",
-            },
-          },
-        },
-      },
-      filters = {
-        dotfiles = true,
-        custom = { "Icon\r" }, -- Hides MacOS Icon files
-        exclude = { }
-      },
-    }
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("nvim-tree").setup({
+			reload_on_bufenter = true,
+			sync_root_with_cwd = true,
+			view = {
+				signcolumn = "yes",
+			},
+			actions = {
+				change_dir = {
+					restrict_above_cwd = true,
+				},
+			},
+			renderer = {
+				add_trailing = false,
+				special_files = { "package.json" },
+				root_folder_label = false,
+				icons = {
+					git_placement = "signcolumn",
+					show = {
+						file = false,
+						folder = false,
+						folder_arrow = true,
+						git = true,
+						bookmarks = false,
+					},
+					glyphs = {
+						default = "",
+						symlink = "",
+						bookmark = "",
+						git = {
+							unstaged = "●",
+							staged = "+",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "?",
+							deleted = "-",
+							ignored = "◌",
+						},
+					},
+				},
+			},
+			filters = {
+				dotfiles = true,
+				custom = { "Icon\r" }, -- Hides MacOS Icon files
+				exclude = {},
+			},
+		})
 
-    -- Keymaps
-    local keymap = vim.keymap
-    keymap.set('n', '<Leader>n', ':NvimTreeFindFile<CR>', {desc="Open file tree to the current file", silent = true})
-    keymap.set('n', '<Leader>d', ':NvimTreeToggle<CR>', {desc = "Toggle file tree", silent = true})
-
-  end 
+		-- Keymaps
+		local api = require("nvim-tree.api")
+		local keymap = vim.keymap
+		keymap.set(
+			"n",
+			"<Leader>n",
+			":NvimTreeFindFile<CR>",
+			{ desc = "Open file tree to the current file", silent = true }
+		)
+		keymap.set("n", "<Leader>d", ":NvimTreeToggle<CR>", { desc = "Toggle file tree", silent = true })
+		keymap.set("n", "<Enter>", api.tree.change_root_to_node, { desc = "CD file tree", silent = true })
+		keymap.set("n", "?", api.tree.toggle_help, { desc = "Toggle nvim-tree help", silent = true })
+	end,
 }
