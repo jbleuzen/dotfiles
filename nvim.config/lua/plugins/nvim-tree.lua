c7500c10645bf4fd81180782c235e2a6fd84945a
Johan Bleuzen <johan@bleuzen.fr>
2025-03-12 17:22:06 +0100

    feat: Sync CWD with nvim-tree current visible folder

--- nvim.config/lua/plugins/nvim-tree.lua
+++ nvim.config/lua/plugins/nvim-tree.lua
@@ -2,9 +2,49 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
+		local api = require("nvim-tree.api")
+
+		-- Sync cwd with nvim-tree folder
+		local function nvim_tree_on_attach(bufnr)
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+			--
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+			--
+			-- custom mappings
+			local function change_root_to_node(node)
+				if node == nil then
+					node = api.tree.get_node_under_cursor()
+				end
+				if node ~= nil and node.type == "directory" then
+					vim.api.nvim_set_current_dir(node.absolute_path)
+				end
+				api.tree.change_root_to_node(node)
+			end
+
+			local function change_root_to_parent(node)
+				local abs_path
+				if node == nil then
+					abs_path = api.tree.get_nodes().absolute_path
+				else
+					abs_path = node.absolute_path
+				end
+
+				local parent_path = vim.fs.dirname(abs_path)
+				vim.api.nvim_set_current_dir(parent_path)
+				api.tree.change_root(parent_path)
+			end
+
+			vim.keymap.set("n", "<Space>", change_root_to_node, opts("CD"))
+			vim.keymap.set("n", "-", change_root_to_parent, opts("Up"))
+		end
+
 		require("nvim-tree").setup({
+			on_attach = nvim_tree_on_attach,
 			reload_on_bufenter = true,
-			sync_root_with_cwd = true,
+			-- sync_root_with_cwd = true,
 			view = {
 				signcolumn = "yes",
 			},
@@ -50,7 +90,6 @@ return {
 		})
 
 		-- Keymaps
-		local api = require("nvim-tree.api")
 		local keymap = vim.keymap
 		keymap.set(
 			"n",
@@ -59,7 +98,34 @@ return {
 			{ desc = "Open file tree to the current file", silent = true }
 		)
 		keymap.set("n", "<Leader>d", ":NvimTreeToggle<CR>", { desc = "Toggle file tree", silent = true })
-		keymap.set("n", "<Enter>", api.tree.change_root_to_node, { desc = "CD file tree", silent = true })
+		-- keymap.set("n", "<Space>", api.tree.change_root_to_node, { desc = "CD file tree", silent = true })
 		keymap.set("n", "?", api.tree.toggle_help, { desc = "Toggle nvim-tree help", silent = true })
+
+		-- -- custom mappings
+		-- local function change_root_to_node(node)
+		-- 	if node == nil then
+		-- 		node = api.tree.get_node_under_cursor()
+		-- 	end
+		-- 	if node ~= nil and node.type == "directory" then
+		-- 		vim.api.nvim_set_current_dir(node.absolute_path)
+		-- 	end
+		-- 	api.tree.change_root_to_node(node)
+		-- end
+		--
+		-- local function change_root_to_parent(node)
+		-- 	local abs_path
+		-- 	if node == nil then
+		-- 		abs_path = api.tree.get_nodes().absolute_path
+		-- 	else
+		-- 		abs_path = node.absolute_path
+		-- 	end
+		--
+		-- 	local parent_path = vim.fs.dirname(abs_path)
+		-- 	vim.api.nvim_set_current_dir(parent_path)
+		-- 	api.tree.change_root(parent_path)
+		-- end
+		--
+		-- keymap.set("n", "<Space>", change_root_to_node, { desc = "CD", silent = true })
+		-- keymap.set("n", "-", change_root_to_parent, { desc = "Up", silent = true })
 	end,
 }
