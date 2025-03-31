801402fd3aeb274754d50ea475cc391aeda4d4fc
Johan Bleuzen <johan@bleuzen.fr>
2025-03-31 10:56:43 +0200

    feat: Update nvim-cmp parameters

--- nvim.config/lua/plugins/nvim-cmp.lua
+++ nvim.config/lua/plugins/nvim-cmp.lua
@@ -22,7 +22,18 @@ return {
 				native_menu = false,
 			},
 			formatting = {
-				format = lspkind.cmp_format({ with_text = false, maxwidth = 50, ellipsis_char = "…" }),
+				format = lspkind.cmp_format({
+					mode = "symbol_text", -- show only symbol annotations
+					maxwidth = {
+						-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+						-- can also be a function to dynamically calculate max width such as
+						-- menu = function() return math.floor(0.45 * vim.o.columns) end,
+						menu = 50, -- leading text (labelDetails)
+						abbr = 50, -- actual suggestion item
+					},
+					ellipsis_char = "…", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+				}),
 			},
 			snippet = {
 				expand = function(args)
