345d924b66c77beeb41558def217befe0e078666
Johan Bleuzen <johan@bleuzen.fr>
2025-05-19 04:54:07 +0200

    feat: Restore LuaSnip

--- nvim.config/lua/plugins/nvim-cmp.lua
+++ nvim.config/lua/plugins/nvim-cmp.lua
@@ -5,7 +5,7 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-nvim-lua",
-		-- "L3MON4D3/LuaSnip",
+		"L3MON4D3/LuaSnip",
 		"onsails/lspkind-nvim",
 	},
 	config = function()
