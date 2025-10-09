8b6ad6553aa98911d36d34a205fa52ce44562fa3
Johan Bleuzen <johan@bleuzen.fr>
2025-10-09 22:19:33 +0200

    chore: Update mason repo organization

--- nvim.config/lua/plugins/mason.lua
+++ nvim.config/lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
-	"williamboman/mason.nvim",
+	"mason-org/mason.nvim",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
 	},
 	config = function()
